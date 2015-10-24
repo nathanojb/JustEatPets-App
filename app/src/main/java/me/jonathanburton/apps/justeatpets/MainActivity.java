a66eafb04b012df6faf262cd9482bb67bac25e57
Nathanojb <jd-burton@hotmail.co.uk>
2015-10-24 17:26:16 +0100

    Saved Image as well as display it

--- app/src/main/java/me/jonathanburton/apps/justeatpets/MainActivity.java
+++ app/src/main/java/me/jonathanburton/apps/justeatpets/MainActivity.java
@@ -3,14 +3,22 @@ package me.jonathanburton.apps.justeatpets;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "JUSTEATYOURPETS";
     static final int REQUEST_IMAGE_CAPTURE = 1;
     Button shootButton;
     ImageView viewImage;
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             viewImage.setImageBitmap(imageBitmap);
+            storeImage(imageBitmap);
 
         }
     }
@@ -44,6 +53,58 @@ public class MainActivity extends AppCompatActivity {
     private void sendToClarify() {
         //TODO: implement clarify api
     }
+
+
+
+
+
+
+
+    private void storeImage(Bitmap image) {
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            Log.d(TAG,
+                    "Error creating media file, check storage permissions: ");// e.getMessage());
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
+    /** Create a File for saving an image or video */
+    private File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        File mediaFile;
+        String mImageName="temp" + ".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
+    }
+
+
+
+
 
 
 }
